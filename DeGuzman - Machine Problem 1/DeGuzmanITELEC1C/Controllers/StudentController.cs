cae88c64ab55c2267ea374bfabd0c7ee9a86ae74
Emman-DG <mikhailemman.deguzman.cics@ust.edu.ph>
2023-10-31 15:30:19 +0800

    Machine Problem 1

---
+++ DeGuzman - Machine Problem 1/DeGuzmanITELEC1C/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeGuzmanITELEC1C.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddStudent() 
+        {
+            return View();
+        }
+    }
+}
